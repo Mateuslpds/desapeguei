df8e80562aefeb2696d8e08ddcfc0cda317ee021
ianq1w1 <ianpereiraian2@gmail.com>
2022-11-15 01:53:24 -0300

    pequenos ajustes no post object e no html da tela de doação

---
+++ back/post-object.php
@@ -0,0 +1,54 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+header('Content-Type: application/json');
+
+if(!isset($_POST['nome'])){err('nome esta faltando', __LINE__);}
+if(!isset($_POST['descricao'])){err('descricao esta faltando', __LINE__);}
+if(!isset($_POST['foto'])){err('foto esta faltando',__LINE__);}
+
+require_once(__DIR__.'/protected/database.php');
+
+try{
+    $q = $db->prepare('INSERT INTO obj VALUES (null, :nome, :descricao, :foto)');
+    $q->bindValue(':nome', $_POST['nome']);
+    $q->bindValue(':descricao', $_POST['descricao']);
+    $q->bindValue(':foto', $_POST['foto']);
+    $q->execute();
+    $objId = $db->lastInsertId();
+}catch(PDOException $ex){
+    err('nao foi possivel postar a doação', __LINE__);
+}
+
+echo '{"status":1, "message": "doação postada", "id":"'.$objId.'"}';
+
+function err($message = 'error', $debug = 0){
+    echo '{
+        "status":0,
+        "message":"'.$message.'",
+        "debug":'.$debug.'
+    }';
+    exit();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- front/src/Routes/DoarTela.svelte
+++ front/src/Routes/DoarTela.svelte
@@ -1,35 +1,72 @@
 <script>
     import Menu from "./Menu.svelte";
+
+    let obj = {};
+
+    const postOBJ = async () => {
+        const updateRoute = "http://localhost/desapeguei/back/post-object.php";
+        const dado = new FormData();
+        dado.append("nome", obj.nome);
+        dado.append("descrição", obj.descricao);
+        dado.append("foto", obj.foto);
+
+        let res = await fetch(updateRoute, {
+            method: "POST",
+            body: dado,
+        });
+    };
 </script>
 
 <link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css"
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css"
 />
 
 <main>
     <Menu />
 </main>
 
-<body class ="container  shadow-lg mt-5" id="BOXdoar">
+<body class="container  shadow-lg mt-5" id="BOXdoar">
     <form class="container row gy-5" id="BOXformgeral">
-    <div class="form-group col-6">
-        <label for="image">Adicione foto ao objeto</label>
-        <input type="file" class="form-control" placeholder="escolha a foto em seu dispositivo">
-    </div>
-    <div class="form-group col-6">
-        <label for="name">Insira o nome do objeto</label>
-        <input type="text" class="form-control"  placeholder="nome do objeto...">
-    </div>
-    <div class="form-group col-6">
-        <label for="dsrc"> Descrição</label>
-        <input type="text" id="DOARdescri" class="form-control"  placeholder="insira aqui a descrição do objeto...">
-    </div>
-    <div class="form-group col-6">
-        <label for="dsrc"> Tipo do produto</label>
-         <select name="tipo">
-            
-         </select>
-    </div>
+        <div class="form-group col-6">
+            <label for="image">Adicione foto ao objeto</label>
+            <input
+                type="file"
+                name="foto"
+                id="foto"
+                class="form-control"
+                placeholder="escolha a foto em seu dispositivo"
+                bind:value={obj.foto}
+            />
+        </div>
+        <div class="form-group col-6">
+            <label for="name">Insira o nome do objeto</label>
+            <input
+                type="text"
+                name="nome"
+                id="nome"
+                class="form-control"
+                placeholder="nome do objeto..."
+                bind:value={obj.nome}
+            />
+        </div>
+        <div class="form-group col-6">
+            <label for="dsrc"> Descrição</label>
+            <input
+                type="text"
+                name="descricao"
+                id="DOARdescri"
+                class="form-control"
+                placeholder="insira aqui a descrição do objeto..."
+                bind:value={obj.descricao}
+            />
+        </div>
+        <div class="form-group col-6">
+            <label for="dsrc"> Tipo do produto</label>
+            <select name="tipo" />
+        </div>
+        <div>
+            <button on:click={postOBJ} type="submit">Fazer doação</button>
+        </div>
     </form>
 </body>
