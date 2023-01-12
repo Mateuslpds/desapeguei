01f468d5c19cfc5c107ef0c09c674e3abe98e654
Carolayne <carolwolf0101@gmail.com>
2023-01-11 22:00:53 -0300

    Pequenas alterações feitas no front da caixinha e na tela de doação.

--- front/src/Routes/DoarTela.svelte
+++ front/src/Routes/DoarTela.svelte
@@ -55,51 +55,47 @@
 </main>
 <body class="box">
     <h2>Faça sua doação</h2>
-    <form on:submit|preventDefault={postOBJ}  class="container row gy-5" id="BOXformgeral" >
-        <div class="form-group col-6">
-            <label for="imagem">Adicione imagem ao objeto</label>
+    <form on:submit|preventDefault={postOBJ}  class="geral" id="BOXformgeral" >
+        <div class="titulo">
+            <label for="titulo">Nome do objeto</label>
             <input
                 type="text"
+                name="titulo"
+                id="titulo"
+                class="form-control border border-secondary p-1"
+                placeholder="Insira o nome do objeto"
+            />
+        <div class="inpimage">
+            <label for="imagem">Adicione imagem ao objeto</label>
+            <input
+                type="file"
                 name="imagem"
                 id="imagem"
                 class="form-control border border-secondary p-1"
-                placeholder="escolha a imagem em seu dispositivo"
+                placeholder="Insira uma imagem."
                 bind:value={imagem}
             />
         </div>
-        <div class="form-group col-6">
-            <label for="dsrc"> Descrição</label>
-            <input
-                type="text"
+        <div class="inpdescri">
+            <label for="dsrc">Descrição</label>
+            <textarea 
                 name="descricao"
                 id="DOARdescri"
-                class="form-control border border-secondary pt-3"
-                placeholder="insira aqui a descrição do objeto..."
+                class="form-control border border-secondary p-1"
+                placeholder="Insira aqui a descrição do objeto."
                 bind:value={descricao}
             />
-        </div>
 
-        <select name="tipo" id="tipo" bind:value={tipo}>
+        </div>
+        <label class="tipo" for="tipo">Tipo</label>
+        <select class="tipobutton" name="tipo" id="tipo" bind:value={tipo}>
             {#each types as type}
                 <option value={type.TIPO_ID}>{type.TIPO_DESCRICAO}</option>
             {/each}
         </select>
-        
+
         <div>
-            <button class="btn btn-primary">Fazer doação</button>
+            <button class="botão">Fazer doação</button>
         </div>
     </form>
 </body>
-
-
-<style>
-    h2 {
-        margin-top: 2rem;
-        margin-bottom: -4rem;
-        text-align: center;
-    }
-    label {
-        line-height: 10px;
-    }
-
-</style>
