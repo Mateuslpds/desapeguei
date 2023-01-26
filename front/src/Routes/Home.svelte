a96c4ed87a1aa582e41b8d16fdad5f5a9159aa46
Mateus Lopes <mateusyasmim2014@gmail.com>
2023-01-25 22:18:24 -0300

    Adicionando carrosséis na home

--- front/src/Routes/Home.svelte
+++ front/src/Routes/Home.svelte
@@ -3,6 +3,15 @@
   import { onMount } from "svelte";
   import Menu from "./Menu.svelte";
 
+  import { Navigation } from "swiper";
+
+  // Import Swiper Svelte components
+  import { Swiper, SwiperSlide } from "swiper/svelte";
+
+  // Import Swiper styles
+  import "swiper/css";
+  import "swiper/css/navigation";
+
   let objs = [];
 
   const imgPath = "http://localhost/desapeguei/back/imagens/";
@@ -20,7 +29,8 @@
   });
 
   const getOBJ = async (obj) => {
-    const loadRoute = "http://localhost/desapeguei/back/get-session-obj.php?obj=" + obj;
+    const loadRoute =
+      "http://localhost/desapeguei/back/get-session-obj.php?obj=" + obj;
     await fetch(loadRoute, {
       credentials: "include",
     });
@@ -33,11 +43,8 @@
   href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css"
 />
 
-<main>
-  <Menu />
-</main>
-
 <body>
+  <Menu />
   {#if localStorage.getItem("isLogged") != undefined}
     <div
       class="col-12 col-lg-auto mb-0 mb-lg-3 me-lg-6 w-25  pl-lg-5"
@@ -50,19 +57,105 @@
       </a>
     </div>
   {/if}
-
-  {#each objs as obj}
-    <div>
-      {obj.OBJ_DESCRICAO}
-      <img src="{imgPath}{obj.OBJ_IMG}" alt="">
-      <a href="/agendamento" use:link>
-        <button on:click={() => getOBJ(obj.OBJ_ID)}> agendar</button>
-      </a>
-    </div>
-  {/each}
+  <h1>Recentes</h1>
+  <Swiper
+    modules={[Navigation]}
+    spaceBetween={10}
+    slidesPerView={7}
+    rewind={true}
+    navigation
+    on:slideChange={() => console.log("slide change")}
+    on:swiper={(e) => console.log(e.detail[0])}
+  >
+    {#each objs.slice(0, 14).reverse() as obj}
+      <SwiperSlide>
+        <a
+          class="card"
+          on:click={() => getOBJ(obj.OBJ_ID)}
+          href="/agendamento"
+          use:link
+        >
+          <img
+            class="objImage"
+            src="{imgPath}{obj.OBJ_IMG}"
+            alt="Avatar"
+            style="width:100%"
+          />
+          <div class="container">
+            <h5><b>Nome do Objeto</b></h5>
+            <p class="descricao">{obj.OBJ_DESCRICAO}</p>
+          </div>
+        </a>
+      </SwiperSlide>
+    {/each}
+  </Swiper>
+  <h1>Tipo 1</h1>
+  <Swiper
+    modules={[Navigation]}
+    spaceBetween={10}
+    slidesPerView={7}
+    rewind={true}
+    navigation
+    on:slideChange={() => console.log("slide change")}
+    on:swiper={(e) => console.log(e.detail[0])}
+  >
+    {#each objs.filter(obj => obj.OBJ_TIPO_ID == 1).slice(0, 14) as obj}
+      <SwiperSlide>
+        <a
+          class="card"
+          on:click={() => getOBJ(obj.OBJ_ID)}
+          href="/agendamento"
+          use:link
+        >
+          <img
+            class="objImage"
+            src="{imgPath}{obj.OBJ_IMG}"
+            alt="Avatar"
+            style="width:100%"
+          />
+          <div class="container">
+            <h5><b>Nome do Objeto</b></h5>
+            <p class="descricao">{obj.OBJ_DESCRICAO}</p>
+          </div>
+        </a>
+      </SwiperSlide>
+    {/each}
+  </Swiper>
+  <h1>Tipo 2</h1>
+  <Swiper
+    modules={[Navigation]}
+    spaceBetween={10}
+    slidesPerView={7}
+    rewind={true}
+    navigation
+    on:slideChange={() => console.log("slide change")}
+    on:swiper={(e) => console.log(e.detail[0])}
+  >
+    {#each objs.filter(obj => obj.OBJ_TIPO_ID == 2).slice(0, 14) as obj}
+      <SwiperSlide>
+        <a
+          class="card"
+          on:click={() => getOBJ(obj.OBJ_ID)}
+          href="/agendamento"
+          use:link
+        >
+          <img
+            class="objImage"
+            src="{imgPath}{obj.OBJ_IMG}"
+            alt="Avatar"
+            style="width:100%"
+          />
+          <div class="container">
+            <h5><b>Nome do Objeto</b></h5>
+            <p class="descricao">{obj.OBJ_DESCRICAO}</p>
+          </div>
+        </a>
+      </SwiperSlide>
+    {/each}
+  </Swiper>
 </body>
-<div class="titulo">DESCUBRA</div>
 
+<div class="titulo">DESCUBRA</div>
 <div class="row">
   <div class="column">
     <img
@@ -97,6 +190,43 @@
 <style>
   * {
     box-sizing: border-box;
+  }
+
+  body {
+    background-color: #ebebeb;
+  }
+
+  h1 {
+    margin-left: 3.5rem;
+  }
+
+  .card {
+    color: #333;
+    width: 16rem;
+    height: 23rem;
+    box-shadow: 0 0.5px 0px 0 rgba(0, 0, 0, 0.2);
+    margin-top: 0.5rem;
+    margin-bottom: 3rem;
+    margin-left: 3rem;
+    margin-right: 3rem;
+    border-radius: 5px;
+    text-decoration: none;
+  }
+
+  .objImage {
+    margin-bottom: 20px;
+    padding: 20px;
+    height: 17rem;
+  }
+
+  .container {
+    padding: 2px 16px;
+  }
+
+  .descricao {
+    height: 3rem;
+    overflow-wrap: break-word;
+    overflow: hidden;
   }
 
   .row {
