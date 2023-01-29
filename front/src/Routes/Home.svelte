f3b1a2c7a4c245e93f4ec39939fc5e9e6a3366fa
EmersonLima03 <emersonssantos321@gmail.com>
2023-01-28 23:53:01 -0300

    adicionando a Lading Page

--- front/src/Routes/Home.svelte
+++ front/src/Routes/Home.svelte
@@ -57,6 +57,27 @@
       </a>
     </div>
   {/if}
+
+
+
+<main class="conteudo">
+  <section class="conteudo-principal">
+    <div class="conteudo-principal-escrito">
+      <h1 class="conteudo-principal-titulo">Desapeguei Doação</h1>
+      <h2 class="conteudo-principal-subtitulo">Preserve o mundo com a sua doação de eletrônicos.</h2>
+      <button class="conteudo-principal-escrito-botao">Faça uma doação!</button>
+    </div>
+      <img class="conteudo-principal-imagem" src="imagens/robo.svg" alt="">
+  </section>
+
+  <section class="conteudo-secundario">
+    <h3 class="conteudo-secundario-titulo">O que a doação de eletrônicos faz por você?</h3>
+      <p class="conteudo-secundario-paragrafo">1. Doar ou reciclar eletrônicos de consumo preserva nossos recursos naturais e evita a poluição do ar e da água, bem como a emissão de gases.</p>
+      <p class="conteudo-secundario-paragrafo">2. A reciclagem de um milhão de laptops economiza a energia equivalente à eletricidade usada por mais de 3.500 residências em um ano. </p>
+      <p class="conteudo-secundario-paragrafo">3. Para cada milhão de telefones celulares que reciclarmos, 15 mil kg de cobre, 350 kg de prata, 34 kg de ouro e 15 kg de paládio podem ser recuperadas.</p>
+  </section>
+</main>
+
   <h1>Recentes</h1>
   <Swiper
     modules={[Navigation]}
@@ -186,6 +207,64 @@
     <p>Microfone</p>
   </div>
 </div>
+<footer>
+  <div class="container-footer">
+      <div class="row-footer">
+          <!-- footer col-->
+          <div class="footer-col">
+              <h4>Empresa</h4>
+              <ul>
+                  <li><a href="#"> Quem somos </a></li>
+                  <li><a href=""> nossos serviços </a></li>
+                  <li><a href=""> política de privacidade </a></li>
+                  <li><a href=""> programa de afiliados</a></li>
+              </ul>
+          </div>
+          <!--end footer col-->
+          <!-- footer col-->
+          <div class="footer-col">
+              <h4>Obter ajuda</h4>
+              <ul>
+                  <li><a href="#">FAQ</a></li>
+                  <li><a href="#">Transporte</a></li>
+                  <li><a href="#">devoluções</a></li>
+                  <li><a href="#">Status De Pedido</a></li>
+                
+              </ul>
+          </div>
+          <!--end footer col-->
+          <!-- footer col-->
+          <div class="footer-col">
+              <h4>Loja online</h4>
+              <ul>
+                  <li><a href="#">CPUs</a></li>
+                  <li><a href="#">Impressoras</a></li>
+                  <li><a href="#">Computadores</a></li>
+                  <li><a href="#">Endereço</a></li>
+              </ul>
+          </div>
+          <!--end footer col-->
+          <!-- footer col-->
+          <div class="footer-col">
+             
+                 
+             
+
+              <div class="medias-socias">
+                  <a href="#"> <i class="fa fa-facebook"></i> </a>
+                  <a href="#"> <i class="fa fa-instagram"></i> </a>
+                  <a href="#"> <i class="fa fa-twitter"></i> </a>
+                  <a href="#"> <i class="fa fa-linkedin"></i> </a>
+              </div>
+
+          </div>
+          <!--end footer col-->
+      </div>
+  </div>
+</footer>
+
+
+ 
 
 <style>
   * {
