ea74c08e0761128d481e952e99476da8c568d91e
Mateus Lopes <mateusyasmim2014@gmail.com>
2023-02-01 22:02:08 -0300

    Melhorando o front da tela de agendamento

--- back/get-receptor-confirmation.php
+++ back/get-receptor-confirmation.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION["user"])) {
 
 $AgdStatus = "fechado";
 
-$stmt = $conn->prepare('UPDATE agenda SET AGD_STATUS = :StatusAgenda WHERE AGD_ID = :id AND AGD_STATUS != "aberto"');
+$stmt = $conn->prepare('UPDATE agenda SET AGD_OBJ_ID = null, AGD_STATUS = :StatusAgenda WHERE AGD_ID = :id AND AGD_STATUS != "aberto"');
 $stmt->bindValue(':StatusAgenda', $AgdStatus);
 $stmt->bindValue(':id', $_GET['id']);
 $stmt->execute();
--- front/src/Routes/Agenda.svelte
+++ front/src/Routes/Agenda.svelte
@@ -40,7 +40,7 @@
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css"
 />
-<Menu></Menu>
+
 <head>
     <title>ViaCEP Webservice</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -109,41 +109,112 @@
             limpa_formulário_cep();
         }
     };
-
     </script>
-    </head>
+</head>
 
-    <body>
-
-      {#each infos as info}
-
-<h1>
-    NOME DO DOADOR: {info.USUARIO_NOME}
-    TELEFONE DE CONTADO DO DOADOR: {info.USUARIO_TEL}
-</h1>
-<img src="{imgPath}{info.OBJ_IMG}" alt="" />
-<h1>Objeto: {info.OBJ_NOME}</h1>
-<h1>Descrição: {info.OBJ_DESCRICAO}</h1>
-{/each}
-
-<h1>Realize seu agendamento</h1>
-
-<form on:submit|preventDefault={agendaCreate}>
-    <div>
-        <label for="">Insira o CEP</label>
-        <input type="text" required minlength="8" maxlength="9"  bind:value={CEP} onchange="pesquisacep(this.value);" > <br/>
-         <label>Rua:
-        <input name="rua" type="text" id="rua" size="60" /></label><br />
-        <label>Bairro:
-        <input name="bairro" type="text" id="bairro" size="40" /></label><br />
-        <label>Cidade:
-        <input name="cidade" type="text" id="cidade" size="40" /></label><br />
-        <label>Estado:
-        <input name="uf" type="text" id="uf" size="2" /></label><br />
+<body>
+    <Menu></Menu>
+    <div class="container">
+        <div class="obj-box">
+        {#each infos as info}
+            <img class="obj-image" src="{imgPath}{info.OBJ_IMG}" alt="" />
+            <div class="obj-info">
+                <h3 class="obj-name">{info.OBJ_NOME}</h3>
+                <p class="obj-desc">Descrição: {info.OBJ_DESCRICAO}</p>
+                <div class="user-info">                
+                    <h5>Informações do Doador: </h5>
+                    <h6>Nome: {info.USUARIO_NOME}</h6>
+                    <h6>E-mail: {info.USUARIO_EMAIL}</h6>
+                    <h6>Telefone: {info.USUARIO_TEL}</h6>
+                </div>
+            </div>
+        {/each}
+        </div>
+        <div class="agd-box">
+            <h1 class="agd-title">Realize seu agendamento</h1>
+            <form class="agd-form" on:submit|preventDefault={agendaCreate}>
+                <label for="">Insira o CEP</label>
+                <input type="text" required minlength="8" maxlength="9"  bind:value={CEP} onchange="pesquisacep(this.value);" >
+                <div class="cep-results">
+                    <label style="float: left; width: 30%">Rua:
+                    <input name="rua" type="text" id="rua" size="60" /></label>
+                    <label style="float: left; width: 30%">Bairro:
+                    <input name="bairro" type="text" id="bairro" size="40" /></label>
+                    <label style="float: left; width: 30%">Cidade:
+                    <input name="cidade" type="text" id="cidade" size="40" /></label>
+                    <label style="float: left; width: 10%">Estado:
+                    <input name="uf" type="text" id="uf" size="2" /></label>
+                </div>
+                <button class="btn btn-primary">Confirmar agendamento</button>
+            </form>
+        </div>
     </div>
-    <br>
-    <div>
-        <button>Confirmar agendamento</button>
-    </div>
-</form>
-    </body>
+    <br><br><br><br>
+</body>
+
+<style>
+    body{
+        background-color: #ebebeb;
+    }
+
+    .container{
+        margin-top: 5rem;
+        width: 60%;
+        padding: 10px;
+        border-radius: 0.3rem;
+        background-color: white;
+    }
+
+    .obj-box{
+        display: flex;
+    }
+
+    .obj-image{
+        margin: 2rem;
+        width: 50%;
+        object-fit: contain;
+        align-self: flex-start;
+    }
+
+    .obj-info{
+        margin-top: 2rem;
+        margin-right: 2rem;
+        flex: 1 1 auto;
+    }
+
+    .obj-name{
+        font-weight: bold;
+    }
+
+    .obj-desc{
+        line-height: 1.43em;
+        margin-top: 8px;
+        margin-bottom: 1rem;
+        font-size: 16px;
+        color: rgba(0,0,0,.9);;
+        word-break: break-all;
+    }
+
+    .user-info > h5{
+        font-weight: bold;
+    }
+
+    .agd-title{
+        margin-left: 2rem;
+    }
+
+    .agd-form{
+        margin-left: 2rem;
+    }
+
+    .cep-results{
+        padding-right: 3rem;
+        margin-bottom: 2rem;
+        width: 100%;
+    }
+
+    .btn{
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+</style>
